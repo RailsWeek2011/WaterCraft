a506eb4c5fc4e97e40ba321f42562ca9696861bf
Markus Benjamin Kretsch <markus.benjamin.kretsch@mni.thm.de>
2011-07-27 17:14:19 +0200

    Paar Skills

--- app/controllers/fight_controller.rb
+++ app/controllers/fight_controller.rb
@@ -11,24 +11,38 @@ class FightController < ApplicationController
   	while @defense_creature.hp > 0 && @attack_creature.hp > 0 do
   		
   		if @attack_creature.turn
+  		
+  			@attack_creature.preCondition
   			@log += @attack_creature.attack @defense_creature
+  			@attack_creature.postCondition
   			@attack_creature.changeTurn @defense_creature
   		else
+  			@defense_creature.preCondition
   			@log += @defense_creature.attack @attack_creature
+  			@defense_creature.postCondition
   			@defense_creature.changeTurn @attack_creature
   		end
   			@log += "           #{@attack_creature.fish.name}(#{@attack_creature.hp})   #{@defense_creature.fish.name}(#{@defense_creature.hp})<br>"
   	end
   	
-  	if @defense_creature.hp <= 0
+  	if @defense_creature.hp <= 0 && @attack_creature.hp <= 0
+  		@log += "<br> Uentschieden"
+  		@attack_creature.getExp 5
+  		@defense_creature.getExp 5
+  		@attack.lose += 1
+  		@defense.lose += 1
+  		@attack.save
+  		@defense.save
+  	elsif @defense_creature.hp <= 0
   		@log += "<br>#{@attack.nick}s Fish #{@attack_creature.fish.name} gewinnt!"
-  		@attack.win += 1;
   		@attack_creature.getExp 10
+  		@attack.win += 1;
   		@defense.lose += 1;
   		@attack.save
   		@defense.save
   	else
   		@log += "<br>#{@defense.nick}s Fish #{@defense_creature.fish.name} gewinnt!"
+  		@defense_creature.getExp 10
   		@attack.lose += 1;
   		@defense.win += 1;
   		@attack.save
--- app/helpers/fight_helper.rb
+++ app/helpers/fight_helper.rb
@@ -4,7 +4,7 @@ module FightHelper
 end
 
 class Creature
-attr_accessor :blut, :gift, :turn, :fish, :para, :hp
+attr_accessor :blut, :gift, :turn, :fish, :para, :hp, :reduce
 	def initialize fish
 	  	@fish 	= fish
 		@blut   = 0
@@ -12,6 +12,16 @@ attr_accessor :blut, :gift, :turn, :fish, :para, :hp
 		@para  	= 0
 		@hp = @fish.hp
 		@turn	= false
+		@reduce = 2
+		fs = FishSkill.where :fish_id => @fish.id
+		fs.each do |s|
+			tmp = Skill.where :id => s.skill_id, :when => "hited"
+			unless tmp.first.nil?
+				if tmp.first.name == "Dehnbarkeit"
+					@reduce = 3
+				end
+			end
+		end
 	end
 	
 	def getExp i
@@ -48,7 +58,7 @@ attr_accessor :blut, :gift, :turn, :fish, :para, :hp
 	end
 	
 	def miss
-		return rand(20+@fish.dex)
+		return rand(20+@fish.init)
 	end
 	
 	def attack def_creature
@@ -56,10 +66,88 @@ attr_accessor :blut, :gift, :turn, :fish, :para, :hp
 			return "#{def_creature.fish.name} ist ausgewichen"
 		elsif miss <= 0
 			return "#{@fish.name} hat verfehlt"
+		elsif @para != 0
+			return "#{def_creature.fish.name} kann nicht handeln"
 		else
-			def_creature.hp -= @fish.str
-			return "#{@fish.name} tacklet #{def_creature.fish.name} und zieht ihm #{@fish.str} HP ab"
+			log = def_creature.hitedCondition self
+			self.hitCondition def_creature
+			red = (def_creature.reduce * def_creature.fish.dev)/2
+			att = @fish.str + 1
+			log += self.selectAttack def_creature, att, red
+			return log
 		end
+	end
+	
+	def selectAttack def_creature, dmg, red
+		fs = FishSkill.where("points > 0").where(:fish_id => @fish.id)
+		log = ""
+		skills = [ "Tackle", "Tackle" ]
+		fs.each do |s|
+			tmp = Skill.where :id => s.skill_id, :when => "att"
+			unless tmp.first.nil?
+				puts tmp.first.name
+				skills += [ "#{tmp.first.name}" ]
+			end
+		end
+		puts skills
+		att = skills[rand(skills.size)]
+		if att == "Tackle"
+			dmg = dmg -red
+			def_creature.hp -= dmg
+			return "#{@fish.name} #{att} #{def_creature.fish.name} und zieht ihm #{dmg} HP(#{red} Wiederstanden) ab#{log}"
+		elsif att == "Blitz"
+			dmg = dmg
+			def_creature.hp -= dmg
+			return "#{@fish.name} #{att} #{def_creature.fish.name} und zieht ihm #{dmg} HP(#{red} Wiederstanden) ab#{log}"
+		elsif att == "Vergeltung"
+			dmg -= red - 6
+			if dmg <= 0 
+				dmg = 1
+			end
+			def_creature.hp -= dmg*3
+			
+			log = "#{@fish.name} #{att} #{def_creature.fish.name} und zieht ihm #{dmg} HP(#{red} Wiederstanden) ab#{log}"
+			log =+ "#{@fish.name} #{att} #{def_creature.fish.name} und zieht ihm #{dmg} HP(#{red} Wiederstanden) ab#{log}"
+			log =+ "#{@fish.name} #{att} #{def_creature.fish.name} und zieht ihm #{dmg} HP(#{red} Wiederstanden) ab#{log}"
+			return log
+		end
+	end
+	
+	def preCondition
+		
+	end
+	
+	def hitedCondition fish_attacker
+		fs = FishSkill.where("points > 0").where(:fish_id => @fish.id)
+		log = ""
+		fs.each do |s|
+			tmp = Skill.where :id => s.skill_id, :when => "hited"
+			unless tmp.first.nil?
+				puts tmp.first.name
+				if tmp.first.name == "Dornen"
+					fish_attacker.hp -= 1
+					log += "<br>#{fish_attacker.fish.name} erleidet durch Dornen 1nen Schaden"
+				elsif tmp.first.name == "Aufgeladen"
+					fish_attacker.hp -= 1
+					log += "<br>#{fish_attacker.fish.name} erleidet durch Aufladung 1nen Schaden"
+				elsif tmp.first.name == "Konter"
+					if rand(20) == 0
+						puts "test"
+						log += "<br>Konter:"
+						log += self.attack fish_attacker
+					end
+				end
+			end
+		end
+		return log
+	end
+	
+	def hitCondition fish_attacked
+		
+	end
+	
+	def postCondition
+		
 	end
 	
 	def changeTurn def_creature
